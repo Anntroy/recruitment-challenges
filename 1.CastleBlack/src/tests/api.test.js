5763fc86a66b565dd5dd2702f3a56f2b68aab78b
Anntroy <ana.troyan@gmail.com>
2021-06-03 19:28:05 +0200

    feat: test new valid player can be added

--- 1.CastleBlack/src/tests/api.test.js
+++ 1.CastleBlack/src/tests/api.test.js
@@ -5,6 +5,29 @@ test("players are returned as json", (done) => {
   request(app)
     .get("/api/players")
     .set("Accept", "application/json")
-    .expect("Content-Type", /json/)
+    .expect("Content-Type", /application\/json/)
     .expect(200, done);
+});
+
+test("a valid player can be added", (done) => {
+  const newPlayer = {
+    name: "Cersei Lannister",
+    age: 43,
+    health: 96,
+    bag: [2],
+  };
+
+  request(app)
+    .post("/api/players")
+    .send(newPlayer)
+    .set("Accept", "application/json")
+    .expect("Content-Type", /application\/json/)
+    .expect(201, done);
+
+  request(app).get("/api/players").expect(hasNewPlayer).end(done);
+
+  function hasNewPlayer(res) {
+    const data = res.body.map((player) => player.name);
+    expect(data).toContainEqual(newPlayer.name);
+  }
 });
