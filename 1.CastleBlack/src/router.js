d5b461fc818711286b855280ef03a4e562979778
Anntroy <ana.troyan@gmail.com>
2021-06-04 20:27:25 +0200

    feat: answering questions

--- 1.CastleBlack/src/router.js
+++ 1.CastleBlack/src/router.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get("/health", function(req, res) {
+router.get("/health", function (req, res) {
   res.body = "Up and running";
   // QUESTION: why this endpoint blocks the app?
+  // Because we are not sending any response from this endpoint. We can solve it using res.send("Response") or res.json({ error: "Bad request" });
 });
 
 module.exports = router;
