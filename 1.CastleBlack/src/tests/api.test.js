5d279fb3cade09950d73035a1f58c306ee23935a
Anntroy <ana.troyan@gmail.com>
2021-06-04 09:48:26 +0200

    feat:  all data placed in source file

--- 1.CastleBlack/src/tests/api.test.js
+++ 1.CastleBlack/src/tests/api.test.js
@@ -1,43 +1,37 @@
 const request = require("supertest");
 const app = require("../../app");
+const { newPlayer, uncompleteNewPlayer } = require("../helpers/source");
 
-test("players are returned as json", (done) => {
-  request(app)
-    .get("/api/players")
-    .set("Accept", "application/json")
-    .expect("Content-Type", /application\/json/)
-    .expect(200, done);
+describe("GET api/players", () => {
+  test("players are returned as json", (done) => {
+    request(app)
+      .get("/api/players")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /application\/json/)
+      .expect(200, done);
+  });
 });
 
-test("a valid player can be added", (done) => {
-  const newPlayer = {
-    name: "Cersei Lannister",
-    age: 43,
-    health: 96,
-    bag: [2],
-  };
+describe("POST api/players", () => {
+  test("a valid player can be added", (done) => {
+    request(app)
+      .post("/api/players")
+      .send(newPlayer)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /application\/json/)
+      .expect(201, done);
 
-  request(app)
-    .post("/api/players")
-    .send(newPlayer)
-    .set("Accept", "application/json")
-    .expect("Content-Type", /application\/json/)
-    .expect(201, done);
+    request(app).get("/api/players").expect(hasNewPlayer).end(done);
 
-  request(app).get("/api/players").expect(hasNewPlayer).end(done);
-
-  function hasNewPlayer(res) {
-    const data = res.body.map((player) => player.name);
-    expect(data).toContainEqual(newPlayer.name);
-  }
-});
-
-test("new player without name can not be added", (done) => {
-  const newPlayer = {
-    age: 43,
-    health: 96,
-    bag: [2],
-  };
-
-  request(app).post("/api/players").send(newPlayer).expect(400, done);
+    function hasNewPlayer(res) {
+      const data = res.body.map((player) => player.name);
+      expect(data).toContainEqual(newPlayer.name);
+    }
+  });
+  test("new player without name can not be added", (done) => {
+    request(app)
+      .post("/api/players")
+      .send(uncompleteNewPlayer)
+      .expect(400, done);
+  });
 });
